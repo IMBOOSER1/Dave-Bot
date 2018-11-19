959e87abea8ad5660d4871294e928129e88f41f7
IMBOOSER1 <44923714+IMBOOSER1@users.noreply.github.com>
2018-11-19 20:20:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,15 @@
-// Load up the discord.js library
-const Discord = require("discord.js");
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
 
 // This is your client. Some people call it `bot`, some people call it `self`, 
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
