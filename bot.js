4d6e51b3f9be9410369ca4a2d537c06836de6372
IMBOOSER1 <44923714+IMBOOSER1@users.noreply.github.com>
2018-11-19 20:22:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,6 @@ client.on('message', msg => {
 // This is your client. Some people call it `bot`, some people call it `self`, 
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
 // this is what we're refering to. Your client.
-const client = new Discord.Client();
 
 // Here we load the config.json file that contains our token and our prefix values. 
 const config = require("./config.json");
