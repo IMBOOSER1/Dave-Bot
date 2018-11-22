8cfdb8ac1fc8861ab3332ca6b8fb4e8643c5475a
IMBOOSER1 <44923714+IMBOOSER1@users.noreply.github.com>
2018-11-22 16:44:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,19 +19,6 @@ client.on('message', msg => {
 
 
 
-client.on('ready', () => {
-  console.log('I am ready!');
-});
-
-// Create an event listener for messages
-client.on('message', message => {
-  // If the message is "what is my avatar"
-  if (message.content === 'avatar') {
-    // Send the user's avatar URL
-    message.reply(message.author.avatarURL);
-  }
-});
-
 
 
 
